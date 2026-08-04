c6c6efb73ea1b615b0d604b766ede56fb5efe23c
Thiha Phone Naing <thihaphonenaing@u.nus.edu>
2026-08-05 03:55:26 +0800

    Integrate SonarQube for code quality analysis
    Added SonarQube plugin and configuration for project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("org.sonarqube") version "7.3.1.8318"
+}
+sonar {
+  properties {
+    property("sonar.projectKey", "thihapnaing_smartcart-mobile")
+    property("sonar.organization", "thihapnaing0310")
+  }
 }
